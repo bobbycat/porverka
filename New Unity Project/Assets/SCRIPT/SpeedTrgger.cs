c04a17df54784518fb194da4903144eb2c02ca38
bobbycat <afedotov993@gmail.com>
2018-11-20 12:03:13 +0300

    пробный проект

---
+++ New Unity Project/Assets/SCRIPT/SpeedTrgger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedTrgger : MonoBehaviour {
+
+
+    public Rigidbody Rg { get; private set; }
+
+    void Start()
+    {
+        Rg = GetComponent<Rigidbody>();
+    }
+    void OnTriggerExit (Collider other)
+    {   
+     Rg.AddForce(Vector3.forward * 500 * Time.deltaTime, ForceMode.VelocityChange);
+    }
+}
---
+++ New Unity Project/Assets/SCRIPT/playercontrol.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontrol : MonoBehaviour {
+
+
+    public float speed = 1f;
+
+
+    private void Update()
+    {
+        if (Input.GetButton("MoveRight"))
+        {
+            transform.Translate(transform.right * speed);
+        }
+        if (Input.GetButton("MoveLeft"))
+        {
+            transform.Translate(-transform.right * speed);
+        }
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            transform.Translate(transform.forward * speed);
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            transform.Translate(-transform.forward * speed);
+        }
+    }
+
+}
